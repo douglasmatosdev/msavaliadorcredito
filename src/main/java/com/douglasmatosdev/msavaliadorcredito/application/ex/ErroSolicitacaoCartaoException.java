f7b666df190054ebb92b3ddf7d31454ae856e71e
douglasmatosdev <douglasmatosdev@gmail.com>
2024-11-10 18:24:07 -0300

    testing functionalities

---
+++ src/main/java/com/douglasmatosdev/msavaliadorcredito/application/ex/ErroSolicitacaoCartaoException.java
@@ -0,0 +1,7 @@
+package com.douglasmatosdev.msavaliadorcredito.application.ex;
+
+public class ErroSolicitacaoCartaoException extends RuntimeException {
+    public ErroSolicitacaoCartaoException(String message) {
+        super(message);
+    }
+}
